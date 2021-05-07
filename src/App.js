d879703140f9ea96901fecbd841e77bb7aed5e75
Sami Jokhan <jokhans@awty.org>
2021-05-07 13:01:33 -0500

    restarted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Home id="homeBody"/>
+          </Route>
+          <Route path="/about"><About/></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+
+function Home() {
+    return (
+      <>
+
+<h1>Johny's orange juice</h1>
+<div className="Wrapper">
+  <img src="https://draxe.com/wp-content/uploads/2018/02/Orange-Nutrition_FB.jpg" id="image"></img>
+</div>
+        
+      </>
+    );
+  }
+  
+  export default Home;
+  
